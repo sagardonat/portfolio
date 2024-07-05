7458f4706142600d02a22ae21d2d74a8ebbc6ca8
SagarDonut <fighterworld667@gmail.com>
2024-07-05 09:25:52 +0545

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["*.html"],
+  theme: {
+    extend: {
+    //   sticky-top {
+    //     position: sticky;
+    //     top: 0;
+    // }
+
+    },
+  },
+  plugins: [],
+}
+
